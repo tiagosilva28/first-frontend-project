9148164017fe67291343261869173d54d6d479c0
hicalonic <hicalonic@gmail.com>
2023-04-06 17:56:29 +0100

    asdad

--- card.js
+++ card.js
@@ -1,5 +1,8 @@
-
-let innercard, valete, queen, king;
+if (typeof window !== "undefined") {
+let deck = [];
+let p1Deck = [];
+let p2Deck = [];
+let innercard;
 let inner = '<div class="card__inner ';
 let symbol = '<div class="card__symbol ';
 let symbolC = '<div class="card__symbol"></div>';
@@ -44,12 +47,11 @@ function otherCards(value) {
 }
 
 
-
 function createCard (naipe, value) {
   otherCards(value)
     // const element = document.getElementById("p2card");
     let card =` 
-    <section id="1" class="card card--${naipe}" value="${value}">
+    <section class="card card--${naipe}" value="${value}">
           ${innercard}
       </section>`;
   //  element.innerHTML = card;
@@ -58,27 +60,87 @@ function createCard (naipe, value) {
 }
 // createCard("club","A")
 
-let deck = [];
+
 function drawDeck() {
   // let element = document.getElementById("p2card");
   let naipes = ["club", "heart", "diamond","spade"];
   let values = {1:"2",2:"3",3:"4",4:"5",5:"6",6:"7",7:"8",8:"9",9:"10",10:"J",11:"Q",12:"K",13:"A"};
   for(i=0; i < naipes.length; i++) {
     for(j=0; j < Object.keys(values).length; j++) {
-      let cardNova = createCard(naipes[i],Object.values(values)[j])
-      //  element.innerHTML += cardNova;
-      console.log(cardNova);
-      deck.push(cardNova);
+      let newCard = createCard(naipes[i],Object.values(values)[j])
+      //  element.innerHTML += newCard;
+      // console.log(newCard);
+      deck.push(newCard);
     }
   }
+  dealCards(deck);
+
 }
 drawDeck();
-let shuffledArray = deck.sort((a, b) => 0.5 - Math.random());
-for(i=0; i < shuffledArray.length; i++) {
-  let element = document.getElementById("p2card");
-  element.innerHTML += shuffledArray[i];
-}
+console.log(p1Deck);
+console.log(p2Deck);
+
+// function shuffle() {
+// let shuffledArray = deck.sort((a, b) => 0.5 - Math.random());
+// for(i=0; i < shuffledArray.length; i++) {
+//   let element = document.getElementById("p2card");
+//   element.innerHTML += shuffledArray[i];
+// }
+// return shuffledArray;
+// }
+// shuffle();
+
+
+
+function dealCards(deck) {
   
+  // Shuffle the deck using the Fisher-Yates shuffle algorithm
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  
+  // Distribute the cards evenly between the two players
+  for (let i = 0; i < deck.length; i++) {
+    if (i % 2 === 0) {
+      p1Deck.push(deck[i]);
+    } else {
+      p2Deck.push(deck[i]);
+    }
+  }
+  
+  // Return an object containing each player's deck
+  return {
+    player1: p1Deck,
+    player2: p2Deck
+  };
+}
+
+function play() {
+   let element1 = document.getElementById("p1card");
+   let element2 = document.getElementById("p2card");
+   p1 = element1.innerHTML = p1Deck.pop();
+   p2 = element2.innerHTML = p2Deck.pop();
+   war(p1,p2);
+}
+
+function war(p1,p2) {
+let value = p1.slice(p1.indexOf('value="') + 1);
+console.log(value);
+let value2 = p2.slice(p1.indexOf('value="') + 1);
+console.log(value2);
+}
+war(p1,p2);
+
+
+
+
+}
+
+
+
+
+
 
 
 
